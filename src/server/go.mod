4ab26a2451d09716f10dbe6937637675442e3e12
Wartuu <88190400+Wartuu@users.noreply.github.com>
2022-04-22 19:25:49 +0200

    1/2 - /src/server/ - DONE
    added final files for server src
    
    /src/server/data will be changed in final (2/2) commit

---
+++ src/server/go.mod
@@ -0,0 +1,12 @@
+module server
+
+go 1.17
+
+require golang.org/x/net v0.0.0-20220225172249-27dd8689420f
+
+require (
+	github.com/cratonica/2goarray v0.0.0-20190331194516-514510793eaa // indirect
+	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
+)
---
+++ src/server/main.go
@@ -0,0 +1,204 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"golang.org/x/net/websocket"
+	"server/data"
+)
+
+type ControllerMsg struct {
+	Action  string `json:"action"`
+	Details string `json:"details"`
+	Conn    int    `json:"conn"`
+}
+
+type ClientMsg struct {
+	Action  string `json:"action"`
+	Details string `json:"details"`
+}
+
+type ClientResponse struct {
+	Success bool   `json:"success"`
+	Info    string `json:"info"`
+	IsAlive int    `json:"alive"`
+}
+
+type pingResp struct {
+	Alive int `json:"alive"`
+}
+
+var PORT string = ":7000"
+var ToClientInput string
+
+var BroadcastMessage string
+var BroadcastID int64 = 0
+
+func Controller(writer http.ResponseWriter, request *http.Request) {
+	log.Println(request.Header.Get("Sec-Websocket-Protocol"))
+	if request.Header.Get("Sec-Websocket-Protocol") == "7000::" {
+		log.Println("New Controller. Sending socket")
+		ws := websocket.Server{Handler: websocket.Handler(ControllerWs)}
+		ws.ServeHTTP(writer, request)
+	} else {
+		log.Println("Wrong auth")
+	}
+}
+
+func Client(writer http.ResponseWriter, request *http.Request) {
+	log.Println("New Client. Sending socket")
+	ws := websocket.Server{Handler: websocket.Handler(ClientWS)}
+	ws.ServeHTTP(writer, request)
+}
+
+func ClientWS(ws *websocket.Conn) {
+	Connections++
+	log.Println("new Connection succed!!!!")
+	go isAlive(ws)
+
+	OLD := BroadcastID
+
+	for {
+		time.Sleep(1000 * time.Millisecond)
+		if OLD != BroadcastID {
+			log.Println("SENDING TO CLIENT!")
+			websocket.Message.Send(ws, BroadcastMessage)
+			OLD = BroadcastID
+			break
+		}
+
+	}
+}
+
+func ControllerWs(ws *websocket.Conn) {
+	log.Println("NEW CONTROLLER")
+	for {
+		var privateMsg ControllerMsg
+		var message string
+		websocket.Message.Receive(ws, &message)
+		json.Unmarshal([]byte(message), &privateMsg)
+
+		if privateMsg.Conn == 0 {
+			ws.Close()
+			return
+		}
+		if privateMsg.Conn != 0 {
+			if privateMsg.Action == "back" {
+				num--
+			} else if privateMsg.Action == "next" {
+				num++
+			}
+			log.Println(message)
+			numstr := strconv.Itoa(num)
+			BroadcastMessage = strings.Join([]string{"{\"Action\":\"switchnum\", \"Details\":\"", numstr, "\"}"}, "")
+			BroadcastID++
+
+		}
+	}
+}
+
+func isAlive(ws *websocket.Conn) {
+	var ResponsePing ClientResponse
+	var ResponseString string
+	for {
+		time.Sleep(10 * time.Second)
+		websocket.Message.Send(ws, "{\"Action\":\"ping\",\"Details\":\"1\"}")
+
+		websocket.Message.Receive(ws, &ResponseString)
+		json.Unmarshal([]byte(ResponseString), &ResponsePing)
+		if ResponsePing.IsAlive == 0 {
+			Connections--
+			ws.Close()
+			break
+		}
+
+	}
+}
+
+func ControllerSite(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, string(data.Controller))
+}
+
+func ClientSite(writer http.ResponseWriter, request *http.Request) {
+	switch request.URL.Query().Get("page") {
+	case "":
+		fmt.Fprintf(writer, string(data.P1))
+	case "1":
+		fmt.Fprintf(writer, string(data.P1))
+	case "2":
+		fmt.Fprintf(writer, string(data.P2))
+	case "3":
+		fmt.Fprintf(writer, string(data.P3))
+	case "4":
+		fmt.Fprintf(writer, string(data.P4))
+	case "5":
+		fmt.Fprintf(writer, string(data.P5))
+	case "6":
+		fmt.Fprintf(writer, string(data.P6))
+	case "7":
+		fmt.Fprintf(writer, string(data.P7))
+	case "8":
+		fmt.Fprintf(writer, string(data.P8))
+	case "9":
+		fmt.Fprintf(writer, string(data.P9))
+	case "10":
+		fmt.Fprintf(writer, string(data.P10))
+	default:
+		fmt.Fprintf(writer, string(data.P1))
+	}
+
+}
+
+func ControllerJS(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "text/javascript; charset=utf-8")
+	fmt.Fprintf(writer, "%s", string(data.ControllerJS))
+}
+
+func ClientCSS(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "text/css; charset=utf-8")
+	fmt.Fprintf(writer, "%s", data.ClientCSS)
+}
+
+func ClientJS(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "text/javascript; charset=utf-8")
+	fmt.Fprintf(writer, "%s" ,string(data.ClientJS))
+}
+
+func main() {
+	go ConnectionList()
+	http.HandleFunc("/ws/client", Client)
+	http.HandleFunc("/", ClientSite)
+	http.HandleFunc("/ws/controller", Controller)
+	http.HandleFunc("/controller", ControllerSite)
+
+	http.HandleFunc("/js/controller", ControllerJS)
+	http.HandleFunc("/js/client", ClientJS)
+
+	http.HandleFunc("/css/client", ClientCSS)
+
+	log.Println("Running at 127.0.0.1" + PORT)
+
+	err := http.ListenAndServe(PORT, nil)
+
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+var Connections int64 = 0
+
+func ConnectionList() {
+	for {
+		time.Sleep(500 * time.Millisecond)
+		fmt.Println(Connections)
+	}
+
+}
+
+var num = 0
